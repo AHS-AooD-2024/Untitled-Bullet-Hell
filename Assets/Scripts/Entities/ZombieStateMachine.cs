d5c45c8cf502c2fab43c09e0fde349cf2cf8cd63
dxmbname <154003522+dxmbname@users.noreply.github.com>
2025-04-28 08:56:24 -0400

    zombie start
    :D

---
+++ Assets/Scripts/Entities/ZombieStateMachine.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Entities.States;
+
+public class ZombieStateMachine : StateMachine
+{
+    protected override EntityState GetInitialState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
