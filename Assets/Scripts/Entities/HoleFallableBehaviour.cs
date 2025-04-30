603d70f8db6a0aca3ffa36b0ed3c6000054814eb
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2025-04-30 17:21:49 -0400

    Hole stuff

--- Assets/Scripts/Entities/HoleFallableBehaviour.cs
+++ Assets/Scripts/Entities/HoleFallableBehaviour.cs
@@ -9,15 +9,39 @@ namespace Entities {
         private TopDownCharacterControllerBehaviour m_controller;
 
         [SerializeField]
+        private Collider2D m_collider;
+
+        [SerializeField]
         private PositionCache2D m_posCache;
 
         protected PositionCache2D positionCache2D => m_posCache;
 
         public void OnHitByHazard(HazardInstance hazard) {
             // we are immune to falling while dashing/dodge-rolling
-            if(hazard is Hole && !m_controller.IsDashing) {
-                Fall();
+            if(hazard is Hole && !m_controller.IsDashing)
+            {
+                Hole hole = hazard as Hole;
+                if(CanFall(hole)) {
+                    Fall();
+                } else {
+                    StartCoroutine(KeepTryingToFall(hole));
+                }
             }
+        }
+
+        // Without the distance check, taller hitboxes can fall into holes when the feet
+        // are clearly on the ground. This helps minimize that problem.
+        // Another solution would be to skew the hitbox up slightly. Actually, that's probably
+        // better... oh well.
+        private IEnumerator KeepTryingToFall(Hole hole) {
+            do {
+                yield return new WaitForFixedUpdate();
+            } while (!CanFall(hole));
+            Fall();
+        }
+
+        private bool CanFall(Hole hole) {
+            return Vector2.Distance(hole.transform.position, transform.position) <= hole.fallWithinDistance;
         }
 
         protected void StopMoving() {
