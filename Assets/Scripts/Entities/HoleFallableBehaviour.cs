379f8d0e4dab58cd4cd9e877251248b0d96168ac
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2025-04-30 16:53:39 -0400

    Scale falling ?

--- Assets/Scripts/Entities/HoleFallableBehaviour.cs
+++ Assets/Scripts/Entities/HoleFallableBehaviour.cs
@@ -4,42 +4,32 @@ using Util;
 
 namespace Entities {
     [RequireComponent(typeof(PositionCache2D))]
-    public class HoleFallableBehaviour : MonoBehaviour {
-        [SerializeField]
-        private Animator m_animator;
-
+    public abstract class HoleFallableBehaviour : MonoBehaviour {
         [SerializeField]
         private TopDownCharacterControllerBehaviour m_controller;
-        private static readonly int m_fallHash = Animator.StringToHash("Fall");
-
-        private float m_fallLength = -1.0F;
 
         [SerializeField]
         private PositionCache2D m_posCache;
 
+        protected PositionCache2D positionCache2D => m_posCache;
+
         public void OnHitByHazard(HazardInstance hazard) {
             // we are immune to falling while dashing/dodge-rolling
             if(hazard is Hole && !m_controller.IsDashing) {
-                if(m_animator.HasState(0, m_fallHash)) {
-                    m_animator.PlayInFixedTime(m_fallHash);
-
-                    if(m_fallLength < 0.0F) {
-                        m_fallLength = m_animator.GetCurrentAnimatorStateInfo(0).length;
-                    }
-
-                    StartCoroutine(Respawn(m_fallLength, m_posCache.EarliestFrame));
-                } else {
-                    Debug.LogWarning("No \"Fall\" animation found");
-                    StartCoroutine(Respawn(1.0F, m_posCache.EarliestFrame));
-                }
+                Fall();
             }
         }
 
-        private IEnumerator Respawn(float afterSeconds, Vector2 atPosition) {
+        protected void StopMoving() {
             m_controller.Stop();
-            m_controller.enabled = false;
+        }
+
+        protected bool canMove { get => m_controller.enabled; set => m_controller.enabled = value; }
+
+        protected abstract void Fall();
+
+        protected IEnumerator Respawn(float afterSeconds, Vector2 atPosition) {
             yield return new WaitForSeconds(afterSeconds);
-            m_controller.enabled = true;
             transform.position = atPosition;
         }
     }
